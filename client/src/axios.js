47d8f0c3b6b986f1870af65349486608de92c166
PAVAN KUMAR MARPU <pavankumarmarpu23@gmail.com>
2024-07-30 20:31:31 +0530

    updated axios

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 //to send acces token to backend server
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/api/",
+  baseURL: "https://universe-cse.onrender.com/api/",
   withCredentials: true,
 });
